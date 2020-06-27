a6a28c37596ed48b038ce989a5ab77d7d177ee68
Uday Singh Solanki <37234643+udaysolanki4@users.noreply.github.com>
2020-06-27 20:51:30 +0530

    Add files via upload

---
+++ backend/bean/PatientBean.java
@@ -0,0 +1,64 @@
+package com.bean;
+
+import java.util.Date;
+
+public class PatientBean {
+	private int ssn;
+	private String name;
+	private int age;
+	private String doa;
+	private String bedType;
+	private String address;
+	private String state;
+	private String city;
+	
+	public int getSsn() {
+		return ssn;
+	}
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDoa() {
+		return doa;
+	}
+	public void setDoa(String string) {
+		this.doa = string;
+	}
+	public String getBedType() {
+		return bedType;
+	}
+	public void setBedType(String bedType) {
+		this.bedType = bedType;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
