567929e886fb5116e2b7aeb3efa315366bcf3dd3
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2020-06-27 11:34:56 +0530

    Update patientdetails.sql

--- database/patientdetails.sql
+++ database/patientdetails.sql
@@ -1,7 +1,8 @@
 
 create table patient_details
 (
-patient_id int NOT NULL, 
+patient_SSNid int,
+patient_id int NOT NULL AUTO INCREMENT, 
 patient_name varchar(50),
 patient_age int,
 patient_doa int,
@@ -9,11 +10,12 @@ patient_typeofbed varchar(255),
 patient_address varchar(255),
 patient_state varchar(255),
 patient_city varchar(255),
+ patient_status varchar(255),
  PRIMARY KEY (patient_id)
 );
 
-insert into patient_details (patient_id,patient_name,patient_age,patient_doa,patient_typeofbed,patient_address,patient_state,patient_city)
-values(?,?,?,?,?,?,?,?);
+insert into patient_details (patient_SSNid,patient_id,patient_name,patient_age,patient_doa,patient_typeofbed,patient_address,patient_state,patient_city)
+values(?,?,?,?,?,?,?,?,?);
 
 
 select * from patient_details where patient_id=?;
